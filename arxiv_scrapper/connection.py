1f69f4212af02e8d08da26683e20c815227c58bb
Alexander Demidovskij <monadv@yandex.ru>
2023-11-18 18:09:11 +0300

    collected arXiv index without abstracts

---
+++ arxiv_scrapper/connection.py
@@ -0,0 +1,23 @@
+from functools import partial
+
+import requests
+from requests.adapters import HTTPAdapter
+
+from urllib3 import Retry
+
+from arxiv_scrapper.config_dto import Config
+
+
+def make_session(config: Config):
+    session = requests.Session()
+
+    retry = Retry(connect=2)
+    adapter = HTTPAdapter(max_retries=retry)
+
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    session.headers.update(config.get_headers())
+    session.request = partial(session.request, timeout=config.get_timeout())
+    session.verify = False
+
+    return session
--- arxiv_scrapper/main.py
+++ arxiv_scrapper/main.py
@@ -1,88 +1,58 @@
-from functools import partial
+from itertools import product
 from pathlib import Path
+import multiprocessing as mp
 
 import requests
-from requests.adapters import HTTPAdapter
-from tqdm import tqdm
-from urllib3 import Retry
+import pandas as pd
 
 from arxiv_scrapper.config_dto import Config
-from arxiv_scrapper.parser import HTMLParser
+from arxiv_scrapper.connection import make_session
+from arxiv_scrapper.constants import DIST_PATH
+from arxiv_scrapper.parser import SingleMonthParser
 
 
-def make_session(config: Config):
-    session = requests.Session()
+def process_single_month(year, month, session):
+    dump_path = DIST_PATH / f'{year}_{month}.csv'
+    if dump_path.exists():
+        print(f'Skipping {year=} {month=} ...')
+        return
 
-    retry = Retry(connect=2)
-    adapter = HTTPAdapter(max_retries=retry)
+    print(f'Processing {year=} {month=}')
+    parser = SingleMonthParser(session, year, month)
+    month_submissions = parser.run()
 
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-    session.headers.update(config.get_headers())
-    session.request = partial(session.request, timeout=config.get_timeout())
-    session.verify = False
+    if not month_submissions:
+        print(f'No submissions back in {year=} {month=}')
 
-    return session
+    raw = [submission.__dict__ for submission in month_submissions]
+    df = pd.DataFrame(raw)
+    df.to_csv(dump_path)
 
-def make_request(session, url) -> requests.models.Response:
-    """
-    Delivers a response from a request
-    with given configuration
-    """
-    return session.get(url)
-
-
-class TargetURL:
-    base_template = 'https://arxiv.org/list/cs/{year}{month}?skip={cur_page_index}&show={max_links_per_page}'
-    max_links_per_page = 2000
-    def __init__(self, year, month):
-        self._year = year
-        self._month = month
-
-    def build(self, cur_page_index=0) -> str:
-        return self.base_template.format(
-            year=str(self._year)[-2:],
-            month=str(self._month).rjust(2, '0'),  # padding months: 1 -> '01', 11 -> '11',
-            cur_page_index=cur_page_index*self.max_links_per_page,
-            max_links_per_page=self.max_links_per_page
-        )
 
 def main() -> None:  # pragma: no cover
     """
     Entrypoint for scrapper module
     """
+    requests.packages.urllib3.disable_warnings()
+
     CRAWLER_CONFIG_PATH = Path(__file__).parent / 'assets' / 'config.json'
     configuration = Config(CRAWLER_CONFIG_PATH)
     session = make_session(configuration)
 
-    submissions = []
-    for year in tqdm(range(1993, 2023+1)):
-        if year == 1994:
-            # TODO: remove once scales for 1 year
-            break
+    years = range(1993, 2023 + 1)
+    months = range(1, 12 + 1)
+    combinations = list(product(years, months))
 
-        for month in tqdm(range(1, 12+1)):
-            if month == 2:
-                # TODO: remove once scales for 1 month
-                break
+    pool = mp.Pool(mp.cpu_count())
 
-            url = TargetURL(year, month).build()
+    result_objects = (
+        pool.apply_async(process_single_month, args=(year, month, session))
+        for year, month in combinations
+    )
 
-            parser = HTMLParser(url, session)
+    results = (r.get() for r in result_objects)
 
-            max_pages = parser.retrieve_max_pages()
-
-            for page in tqdm(range(max_pages)):
-                if page == 2:
-                    # TODO: remove once scales for 1 page
-                    break
-
-                url = TargetURL(year, month).build(cur_page_index=page)
-                parser = HTMLParser(url, session)
-                submissions.extend(parser.collect_metadata())
-
-    print(f'Collected {len(submissions)} submissions!')
-
+    print(len(list(results)))
 
 
 if __name__ == '__main__':  # pragma: no cover
