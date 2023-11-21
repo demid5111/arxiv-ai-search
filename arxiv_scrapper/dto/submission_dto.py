d571cffdef6516b8f04f3861b9e053d6b97c346a
Alexander Demidovskij <monadv@yandex.ru>
2023-11-22 00:52:35 +0300

    Update submission_dto.py

--- arxiv_scrapper/dto/submission_dto.py
+++ arxiv_scrapper/dto/submission_dto.py
@@ -1,20 +1,37 @@
 from pathlib import Path
-
+import datetime
 import pandas as pd
 
+month_to_num = {
+            'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'may': 5, 'jun': 6,
+            'jul': 7, 'aug': 8, 'sep': 9, 'oct': 10, 'nov': 11, 'dec': 12,
+        }
+NUM_TO_MONTH = {num: month.capitalize() for month, num in month_to_num.items()}
 
 class SubmissionDTO:
     def __init__(self, url, year, month):
         self._url = url
         self._year = year
         self._month = month
+        self._title = None
+        self._abstract = None
+        self._submit_date = None
+        self._authors = None
 
+    def set_title(self, title):
+        self._title = title
 
-def load_submissions(path: Path) -> list[SubmissionDTO]:
-    df = pd.read_csv(path)
+    def set_abstract(self, abstract):
+        self._abstract = abstract
 
-    df['_year'] = df['_year'].astype('int')
+    def set_submit_date(self, date_str: str):
+        raw = date_str.strip().lower().removeprefix('[submitted on ')
+        day, month, year = raw[:raw.index(']')].split()
+        
+        month = month_to_num[month]
 
-    # joint_df.reset_index(inplace=True, drop=True)
+        self._submit_date = datetime.datetime(
+            day=int(day), month=month, year=int(year))
 
-    return df.apply(SubmissionDTO, axis=1).tolist()
+    def set_authors(self, authors):
+        self._authors = authors
