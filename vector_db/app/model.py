00bd9c2bb7a18cb815e55a2647a608181ba6a071
Alexander Demidovskij <monadv@yandex.ru>
2023-11-25 14:45:28 +0300

    [SCRAPPER] Collector for abstracts (#3)

--- vector_db/app/model.py
+++ vector_db/app/model.py
@@ -1,5 +1,6 @@
 from sentence_transformers import SentenceTransformer
 
+
 class Model:
     def __init__(self, model_name: str = 'all-MiniLM-L6-v2') -> None:
         self.model = SentenceTransformer(model_name)
