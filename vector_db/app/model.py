b90bf69ff4f3b173dd21d165e387a3f458f41b0b
Alexander Demidovskij <monadv@yandex.ru>
2023-12-01 19:49:26 +0300

    macos run fixes

--- vector_db/app/model.py
+++ vector_db/app/model.py
@@ -1,9 +1,26 @@
+import torch
+
 from sentence_transformers import SentenceTransformer
 
 
+def get_device():
+    """
+    Get the current torch device.
+
+    :return: The current torch device.
+    """
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else:
+        device = "cpu"
+
+    return device
+
 class Model:
     def __init__(self, model_name: str = 'all-MiniLM-L6-v2') -> None:
-        self.model = SentenceTransformer(model_name, device='cuda')
+        self.model = SentenceTransformer(model_name, device=get_device())
 
     def embedding(self, sentence: str):
         return self.model.encode(sentence)
