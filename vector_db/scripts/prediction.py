fc73ee203a5fc6945deb4f4eb87fc62adf5aa7c5
Salnikov Igor <igorsalnikov74@gmail.com>
2023-12-12 03:07:56 +0300

    Update metrics

--- vector_db/scripts/prediction.py
+++ vector_db/scripts/prediction.py
@@ -3,7 +3,7 @@ import statistics
 from pathlib import Path
 
 import pandas as pd
-from sklearn.metrics import precision_score
+from sklearn.metrics import accuracy_score
 
 from vector_db.app.database import DataBase
 from vector_db.app.model import Model
@@ -34,6 +34,7 @@ for i in range(1, 11):
     columns.append(f'top{i}')
 df = pd.DataFrame(columns=columns)
 
+accuracy = []
 precisions = []
 all_tp = 0
 unrelated = 0
@@ -41,9 +42,10 @@ unrelated = 0
 for row in df_google.iterrows():
     tp = 0
     fp = 0
+    top_k_flag = True
 
     break_flag = 0
-    targets = row[1][2:].to_list()
+    targets = row[1][1:-1].to_list()
 
     for item in targets:
         find = df_full.loc[df_full['_url'] == item]
@@ -55,22 +57,32 @@ for row in df_google.iterrows():
         continue
 
     full_row = df_full.loc[df_full['_url'] == row[1]['query_article_link']]
-    # abstract = full_row['_abstract'].str.replace('Abstract:', '').values[0]
-    abstract = full_row['_title'].values[0]
-    embedding = model.embedding(abstract)
+    text = full_row['_abstract'].str.replace('Abstract:', '').values[0]
+    # text = full_row['_title'].values[0]
+    embedding = model.embedding(text)
     results = db.query_embedding(embedding)
 
     predicts = [item['url'] for item in results['metadatas'][0]]
+    df.loc[len(df)] = [full_row['_url'].values[0]]+[item['url']
+                                                    for item in results['metadatas'][0]]
+    accuracy.append((targets[0], predicts[0]))
     for item in targets:
         if item in predicts:
-            all_tp += 1
+            if top_k_flag:
+                all_tp += 1
+                top_k_flag = False
             tp += 1
         else:
             fp += 1
     precision = tp / (tp + fp)
     precisions.append(precision)
 samples = len(df_google) - unrelated
+df.to_csv('result_all-distilroberta-v1_cosine_.csv')
 
+print('Accuracy: ', accuracy_score(
+    [item[0] for item in accuracy],
+    [item[1] for item in accuracy])
+)
 print('Unrelated samples: ', unrelated)
 print('Target samples: ', samples)
 print('Precisions: ', statistics.mean(precisions))
