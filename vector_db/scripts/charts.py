a8b1273279cfbd3562311b46546e13699b099fbb
Anastasia <70592693+Selifanchik@users.noreply.github.com>
2023-12-19 08:27:42 +0300

    [REPORT] Better colors (#15)

--- vector_db/scripts/charts.py
+++ vector_db/scripts/charts.py
@@ -11,33 +11,38 @@ class Charts():
     def __init__(self, path: Union[str, Path]) -> None:
         self.month_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May',
                            'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        self.dpi = 600
         self.df = pd.read_csv(path)
 
     def all_years(self, save_path: Path = Path('./dist'), start_year: int = 1993, union_year: int = 2011, end_year: int = 2023):
-        labels = []
-        values = []
+        tab20b_cm = [plt.colormaps['tab20b'](v)
+                     for v in np.linspace(0.05, 0.9, 8)]
+        tab20c_cm = [plt.colormaps['tab20c'](v)
+                     for v in np.linspace(0.05, 0.45, 5)]
+        outer_colors = tab20b_cm + tab20c_cm
+        labels_pie = []
+        values_pie = []
         sum_union_year = self.df[self.df._year <= union_year]._year.count()
-        labels.append(f'{start_year}-{union_year}')
-        values.append(sum_union_year)
+        labels_pie.append(f'{start_year}-{union_year}')
+        values_pie.append(sum_union_year)
         for i in range(union_year + 1, end_year + 1):
             sum_year = self.df[self.df._year == i]._year.count()
-            labels.append(str(i))
-            values.append(sum_year)
-        index = 'Year'
-        y_offset = np.zeros(1)
+            labels_pie.append(str(i))
+            values_pie.append(sum_year)
+        plt.clf()
         _, ax = plt.subplots()
-        cmap = plt.colormaps['tab20']
-        outer_colors = cmap(np.arange(13))
-        for row in range(len(values)):
-            ax.bar(index, values[row], label=labels[row], width=0.7,
-                   bottom=y_offset, color=outer_colors[row])
-            y_offset = y_offset + values[row]
-        ax.legend(loc="upper right")
-        ax.spines[['top', 'right']].set_visible(False)
-        ax.set_xlim(-2, 2)
-        plt.savefig(save_path / 'articles_in_every_year.png')
+        ax.pie(values_pie, labels=labels_pie, wedgeprops={
+               'linewidth': 0.5, "edgecolor": 'white'}, colors=outer_colors)
+        plt.savefig(save_path / 'articles_in_every_year.pdf', dpi=self.dpi)
+        plt.close()
 
     def articles_by_months(self, save_path: Path = Path('./dist')):
+        tab20b_cm = [plt.colormaps['tab20b'](v)
+                     for v in np.linspace(0.05, 0.9, 8)]
+        tab20c_cm = [plt.colormaps['tab20c'](v)
+                     for v in np.linspace(0.05, 0.4, 4)]
+        outer_colors = tab20b_cm + tab20c_cm
+        # outer_colors = plt.colormaps['tab20b'](0.25)
         current_month = []
 
         for i in self.month_list:
@@ -46,19 +51,21 @@ class Charts():
             current_month.append(result)
         plt.clf()
         _, ax = plt.subplots()
-        ax.bar(self.month_list, current_month)
+        ax.bar(self.month_list, current_month, color=outer_colors)
         ax.set_xlabel('Months')
         ax.set_ylabel('Count')
         ax.spines[['top', 'right']].set_visible(False)
         ax.grid(axis='both', linestyle='--', linewidth=0.5, color='0.7')
         ax.set_axisbelow(True)
-        plt.savefig(save_path / 'number_of_articles_by_month.png')
+        plt.savefig(save_path / 'number_of_articles_by_month.pdf', dpi=self.dpi)
+        plt.close()
 
     def articles_in_years(self, start_year: int = 1993, end_year: int = 2023, save_path: Path = Path('./dist')):
         labels_plot = []
         values_plot = []
+        sum_year = 0
         for i in range(start_year, end_year + 1):
-            sum_year = self.df[self.df._year == i]._year.count()
+            sum_year += self.df[self.df._year == i]._year.count()
             labels_plot.append(int(i))
             values_plot.append(sum_year)
         plt.clf()
@@ -71,7 +78,8 @@ class Charts():
         ax.xaxis.set_tick_params(rotation=45, labelsize=8)
         ax.spines[['top', 'right']].set_visible(False)
         ax.grid(axis='both', linestyle='--', linewidth=0.5, color='0.7')
-        plt.savefig(save_path / 'number_of_articles_in_years.png')
+        plt.savefig(save_path / 'number_of_articles_in_years.pdf', dpi=self.dpi)
+        plt.close()
 
 
 def get_args():
